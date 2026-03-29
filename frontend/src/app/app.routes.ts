f241b1ca93b57cc5065921459d72379dd64018eb
bzrcerk <lnikbzr@gmail.com>
2026-03-29 19:46:38 +0500

    Added main directories

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import {Dashboard} from './pages/dashboard/dashboard';
+import {Stats} from 'node:fs';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "dashboard",
+    component : Dashboard
+  },
+  {
+    path:"stats",
+    component : Stats
+  },
+  // {
+  //   path:"dashboard/:id",
+  //   component :
+  // }
+  {
+    path:"**",
+    redirectTo:"dashboard"
+  }
+];
