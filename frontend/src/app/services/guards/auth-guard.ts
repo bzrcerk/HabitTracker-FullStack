4a06b328ea2fa2ad6bca659b9d0695414c59a212
bzrcerk <lnikbzr@gmail.com>
2026-04-07 11:26:54 +0500

    added login and register pages.

--- frontend/src/app/services/guards/auth-guard.ts
+++ frontend/src/app/services/guards/auth-guard.ts
@@ -1,5 +1,14 @@
-import { CanActivateFn } from '@angular/router';
+import {CanActivateFn, Router} from '@angular/router';
+import {inject} from '@angular/core';
 
 export const authGuard: CanActivateFn = (route, state) => {
+  // const router = inject(Router);
+  // const token = localStorage.getItem('token')
+  //
+  // if (token) {
+  //   return true;
+  // }
+  //
+  // return router.navigate(['login']);
   return true;
 };
